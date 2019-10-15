f4332bc8f6137dc9228545bcea6232746a0842f5
ezoji <abbas.ezoji@gmail.com>
2019-10-15 23:33:57 +0330

    EDIT: prs_day_base file
    add prs output function to show Diff per personnel

--- prs_day_based.py
+++ prs_day_based.py
@@ -62,7 +62,7 @@ shift_df = shift_df.set_index('Code')
 for prs in chromosom_df.index :       
     chromosom_df.loc[prs] = np.random.choice(shift_df.index.values.tolist(),
                                              size=len(chromosom_df.columns))
-# -----------------------fitness function-------------------------------------# 
+#------------------------fitness function-------------------------------------# 
 def fitness (individual, meta_data):
     prs_count,day_count = individual.shape    
     shift_prs = personnel_df.reset_index()
@@ -75,9 +75,9 @@ def fitness (individual, meta_data):
 #            print('shiftcode:'+str(individual.loc[prs,day+1]))
             shift_lenght += meta_data.loc[individual.loc[prs,day+1]][1]
 
-        shift_lenght_diff.append(shift_lenght - shift_prs.iloc[prs_count,3])
-        shift_prs.set_value(prs_count,4,shift_lenght)
-        shift_prs.set_value(prs_count,7,
+        shift_lenght_diff.append(abs((shift_lenght/shift_prs.iloc[prs_count,3]) - 1))
+        shift_prs.set_value(prs_count,'RequirementWorkMins_real',shift_lenght)
+        shift_prs.set_value(prs_count,'diff',
             abs(shift_lenght - shift_prs.iloc[prs_count,3])
                                )        
 #        print(shift_lenght - shift_prs.iloc[prs_count,3])
@@ -85,8 +85,31 @@ def fitness (individual, meta_data):
         
     cost = np.mean(shift_lenght_diff)
 #    print('cost: ' + str(cost))
-    return cost    
+    return cost  
+# -----------------------prs output function-------------------------------------# 
+def get_personnel_diff_len (individual, meta_data):
+    prs_count,day_count = individual.shape    
+    shift_prs = personnel_df.reset_index()
+    shift_prs['diff'] = 0
+    prs_count = 0
+    shift_lenght_diff = []
+    for prs in personnel_df.index: 
+        shift_lenght = 0          
+        for day in range(day_count):
+#            print('shiftcode:'+str(individual.loc[prs,day+1]))
+            shift_lenght += meta_data.loc[individual.loc[prs,day+1]][1]
 
+        shift_lenght_diff.append(shift_lenght-shift_prs.iloc[prs_count,3])
+        shift_prs.set_value(prs_count,'RequirementWorkMins_real',shift_lenght)
+        shift_prs.set_value(prs_count,'diff',
+            abs(shift_lenght - shift_prs.iloc[prs_count,3])
+                               )        
+#        print(shift_lenght - shift_prs.iloc[prs_count,3])
+        prs_count += 1 
+        
+    cost = np.mean(shift_lenght_diff)
+#    print('cost: ' + str(cost))
+    return cost,shift_prs
 # -----------------------Define GA--------------------------------------------# 
  
 ga = ga.GeneticAlgorithm( seed_data=chromosom_df,
@@ -103,8 +126,7 @@ ga = ga.GeneticAlgorithm( seed_data=chromosom_df,
 ga.fitness_function = fitness               # set the GA's fitness function
 ga.run()                                    # run the GA
 sol_fitness, sol_df = ga.best_individual()
- 
-                    
+sol_fitness, prs_diff_df = get_personnel_diff_len(sol_df,shift_df)
 # ----------------Query for insert shift assignment info----------------------#
 #=============================================================================
 cursor = sql_conn.cursor()
