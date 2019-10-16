065f5af6ff72d352c9249cb57cab68ba26514730
ezoji <abbas.ezoji@gmail.com>
2019-10-16 15:46:11 +0330

    EDIT: tchbichef and mean with together

--- prs_day_based.py
+++ prs_day_based.py
@@ -66,7 +66,7 @@ for prs in chromosom_df.index :
     chromosom_df.loc[prs] = np.random.choice(shift_df.index.values.tolist(),
                                              size=len(chromosom_df.columns))
 #------------------------fitness function-------------------------------------# 
-def fitness (individual, meta_data):
+def fitness_means (individual, meta_data):
     prs_count,day_count = individual.shape    
     shift_prs = personnel_df.reset_index()
     shift_prs['diff'] = 0
@@ -90,7 +90,37 @@ def fitness (individual, meta_data):
 #        print(shift_lenght - shift_prs.iloc[prs_count,3])
         prs_count += 1 
         
-    cost = np.max(shift_lenght_diff)
+ #   cost = np.max(shift_lenght_diff)   # tchebichef role
+    cost = np.mean(shift_lenght_diff) # 
+#    print('cost: ' + str(cost))
+    return cost  
+
+def fitness_tchebechef (individual, meta_data):
+    prs_count,day_count = individual.shape    
+    shift_prs = personnel_df.reset_index()
+    shift_prs['diff'] = 0
+    prs_count = 0
+    shift_lenght_diff = []
+    for prs in personnel_df.index: 
+        shift_lenght = 0          
+        for day in range(day_count):
+#            print('shiftcode:'+str(individual.loc[prs,day+1]))
+            shift_lenght += meta_data.loc[individual.loc[prs,day+1]][1]
+
+        shift_lenght_diff.append(abs(
+                                    (shift_lenght/shift_prs.iloc[prs_count,3]) 
+                                    - 1) 
+                                    * shift_prs.iloc[prs_count,6]
+                                    )
+        shift_prs.set_value(prs_count,'RequirementWorkMins_real',shift_lenght)
+        shift_prs.set_value(prs_count,'diff',
+            abs(shift_lenght - shift_prs.iloc[prs_count,3])
+                               )        
+#        print(shift_lenght - shift_prs.iloc[prs_count,3])
+        prs_count += 1 
+        
+ #   cost = np.max(shift_lenght_diff)   # tchebichef role
+    cost = np.mean(shift_lenght_diff) # 
 #    print('cost: ' + str(cost))
     return cost  
 # -----------------------prs output function----------------------------------# 
@@ -129,7 +159,7 @@ ga = ga.GeneticAlgorithm( seed_data=chromosom_df,
  
  # -----------------------run ga----------------------------------------------# 
  
-ga.fitness_function = fitness               # set the GA's fitness function
+ga.fitness_function = fitness_means               # set the GA's fitness function
 ga.run()                                    # run the GA
 sol_fitness, sol_df = ga.best_individual()
 sol_fitness, prs_diff_df = get_personnel_diff_len(sol_df,shift_df)
