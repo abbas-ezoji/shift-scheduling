9940f55f313bd3a1104881ac368ddb499f808999
ezoji <abbas.ezoji@gmail.com>
2019-10-16 12:42:20 +0330

    CREATE: data modoule
    data access in seprated modoule

---
+++ data_access/sql_server.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import pyodbc
+
+class data(object): 
+    def __init__(self, conn_str, query_gene ,query_personnel ,query_shift):
+        # -------------------- Connection String -----------------------------#
+        self.conn_str = conn_str
+        self.sql_conn = pyodbc.connect(self.conn_str)
+        # ------------------ Query for gene pivoted --------------------------#
+        self.query_gene = query_gene
+        # ----------------- Query for personnel info -------------------------#
+        self.query_personnel = query_personnel        
+        # -------------------Query for shift info-----------------------------#
+        self.query_shift = query_shift        
+        #-------------------------------------------------------------------         
+        self.cursor = self.sql_conn.cursor()                
+               
+    def get_sql_conn(self):
+        sql_conn = self.sql_conn
+        return sql_conn
+        
+    def get_chromosom(self):
+        chromosom_df = pd.read_sql(self.query_gene, self.sql_conn)
+        return chromosom_df
+    
+    def get_personnel(self):
+        personnel_df = pd.read_sql(self.query_personnel,self.sql_conn)    
+        return personnel_df
+    
+    def get_shift(self):
+        shift_df = pd.read_sql(self.query_shift,self.sql_conn)
+        return shift_df
+        
+    def truncate(self):
+        cursor = self.cursor
+        cursor.execute('''truncate table PersonnelShiftDateAssignments''')
+        self.sql_conn.commit()                     
+    
+    def insert_sol(self, sol_df, personnel_df, sol_fitness): 
+        cursor = self.cursor()                
+        year_workingperiod = 1398 * 100 + 3
+        prs_count,day_count = sol_df.shape                      
+        for prs in personnel_df.index:
+            prs_count = prs_count + 1
+            for day in range(day_count): 
+                cursor.execute('''insert into PersonnelShiftDateAssignments 
+                               values (?, ?, ?, ?, ?)'''
+                               ,(prs,int(sol_df.loc[prs][[day+1]])
+                               ,year_workingperiod * 100 + day+1,0,sol_fitness)
+                               )
+         
+        self.sql_conn.commit()                     
+       
+    
+
+
--- prs_day_by_csv.py
+++ prs_day_by_csv.py
@@ -4,22 +4,22 @@ from libs import GA_dataframes as ga
 
 # -----------------------Query for gene pivoted-------------------------------#
 
-chromosom_df = pd.read_csv('output.csv')
+chromosom_df = pd.read_csv('C:/MyFiles/Projects/DM/sample files/output.csv')
 chromosom_df = pd.pivot_table(chromosom_df, values='SHIFTID', 
                               index=['PersonnelBaseId'],
                               columns=['PersianDayOfMonth'], aggfunc=np.sum)
 
 # -----------------------Query for personnel info-----------------------------#
-personnel_df = pd.read_csv('personnel.csv')
+personnel_df = pd.read_csv('C:/MyFiles/Projects/DM/sample files/personnel.csv')
 personnel_df = personnel_df.set_index('PersonnelBaseId')
 # -----------------------Query for shift info---------------------------------#
-shift_df = pd.read_csv('shifts.csv')
+shift_df = pd.read_csv('C:/MyFiles/Projects/DM/sample files/shifts.csv')
 shift_df = shift_df.set_index('Code')
 # -----------------------Randomize gene---------------------------------------#
 for prs in chromosom_df.index :       
     chromosom_df.loc[prs] = np.random.choice(shift_df.index.values.tolist(),size=len(chromosom_df.columns))
 chromosom_df = chromosom_df.astype(int)
-# -----------------------fitness function-------------------------------------# 
+#------------------------fitness function-------------------------------------# 
 def fitness (individual, meta_data):
     prs_count,day_count = individual.shape    
     shift_prs = personnel_df.reset_index()
@@ -32,9 +32,9 @@ def fitness (individual, meta_data):
 #            print('shiftcode:'+str(individual.loc[prs,day+1]))
             shift_lenght += meta_data.loc[individual.loc[prs,day+1]][1]
 
-        shift_lenght_diff.append(shift_lenght - shift_prs.iloc[prs_count,3])
-        shift_prs.set_value(prs_count,4,shift_lenght)
-        shift_prs.set_value(prs_count,7,
+        shift_lenght_diff.append(abs((shift_lenght/shift_prs.iloc[prs_count,3]) - 1))
+        shift_prs.set_value(prs_count,'RequirementWorkMins_real',shift_lenght)
+        shift_prs.set_value(prs_count,'diff',
             abs(shift_lenght - shift_prs.iloc[prs_count,3])
                                )        
 #        print(shift_lenght - shift_prs.iloc[prs_count,3])
@@ -43,7 +43,30 @@ def fitness (individual, meta_data):
     cost = np.mean(shift_lenght_diff)
 #    print('cost: ' + str(cost))
     return cost  
-#--------------------------------------------------------
+# -----------------------prs output function-------------------------------------# 
+def get_personnel_diff_len (individual, meta_data):
+    prs_count,day_count = individual.shape    
+    shift_prs = personnel_df.reset_index()
+    shift_prs['diff'] = 0
+    prs_count = 0
+    shift_lenght_diff = []
+    for prs in personnel_df.index: 
+        shift_lenght = 0          
+        for day in range(day_count):
+#            print('shiftcode:'+str(individual.loc[prs,day+1]))
+            shift_lenght += meta_data.loc[individual.loc[prs,day+1]][1]
+
+        shift_lenght_diff.append(shift_lenght-shift_prs.iloc[prs_count,3])
+        shift_prs.set_value(prs_count,'RequirementWorkMins_real',shift_lenght)
+        shift_prs.set_value(prs_count,'diff',
+            abs(shift_lenght - shift_prs.iloc[prs_count,3])
+                               )        
+#        print(shift_lenght - shift_prs.iloc[prs_count,3])
+        prs_count += 1 
+        
+    cost = np.mean(shift_lenght_diff)
+#    print('cost: ' + str(cost))
+    return cost,shift_prs
 # -----------------------Define GA--------------------------------------------# 
  
 ga = ga.GeneticAlgorithm( seed_data=chromosom_df,
@@ -60,3 +83,4 @@ ga = ga.GeneticAlgorithm( seed_data=chromosom_df,
 ga.fitness_function = fitness               # set the GA's fitness function
 ga.run()                                    # run the GA
 sol_fitness, sol_df = ga.best_individual()
+sol_fitness, prs_diff_df = get_personnel_diff_len(sol_df,shift_df)
